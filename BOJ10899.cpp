2b17770774952857b444af638426bd8a3c7bca89
KPS <kimin1134z@gmail.com>
2025-03-20 22:37:45 +0900

    new file:   BOJ10899-A.cpp 	new file:   BOJ10899.cpp

---
+++ BOJ10899.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main() {
+    long long int p,n;
+    cin >> p >> n;
+    vector<int> v;
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+    int tsum = 0;
+    long long int pnum;
+    for(int i = 0; i < n; i++) {
+        tsum += v[i];
+        if(tsum >= p) {
+            tsum -= v[i];
+            pnum = i;
+            break;
+        }
+        pnum = i+1;
+    }
+    int limit = p-1;
+    long long int ans = 0;
+    for(int i = 0; i < pnum; i++) {
+       ans += limit;
+       limit -= v[i];
+    }
+
+    cout << pnum << ' ' << ans; 
+    
+}
