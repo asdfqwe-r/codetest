5dd18a42162540390c507bca67ded596a34c69cd
asdfqwe-r <kimin1134z@gmail.com>
2026-02-11 03:11:56 +0900

    new file:   BOJ/BOJ1197.cpp 	modified:   BOJ/BOJ14613.cpp 	new file:   BOJ/BOJ1504.cpp 	deleted:    BOJ/BOJ16173.dSYM/Contents/Info.plist 	deleted:    BOJ/BOJ16173.dSYM/Contents/Resources/DWARF/BOJ16173 	deleted:    BOJ/BOJ16173.dSYM/Contents/Resources/Relocations/aarch64/BOJ16173.yml 	deleted:    BOJ/BOJ29700.dSYM/Contents/Info.plist 	deleted:    BOJ/BOJ29700.dSYM/Contents/Resources/DWARF/BOJ29700 	deleted:    BOJ/BOJ29700.dSYM/Contents/Resources/Relocations/aarch64/BOJ29700.yml 	deleted:    BOJ/BOJ31295.dSYM/Contents/Info.plist 	deleted:    BOJ/BOJ31295.dSYM/Contents/Resources/DWARF/BOJ31295 	deleted:    BOJ/BOJ31295.dSYM/Contents/Resources/Relocations/aarch64/BOJ31295.yml

---
+++ BOJ/BOJ1504.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int D[801];
+vector<pair<int,int>> G[801];
+
+void Dijkstra(int s, int t) {
+    memset(D, 0x3f, sizeof D);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+    pq.emplace(D[s] = 0, s);
+
+    while(!pq.empty()) {
+        auto [c,v] = pq.top();
+        pq.pop();
+        if(c == D[v]) {
+            for(auto [i,w] : G[v]) {
+                if(D[i] > c+w) pq.emplace(D[i] = c+w, i);
+            }
+        }
+    }
+    if(D[t] == 0x3f3f3f3f) D[t] = -1;
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N,E;
+    cin >> N >> E;
+
+    for(int i = 0; i < E; i++) {
+        int a,b,c;
+        cin >> a >> b >> c;
+        G[a].push_back({b,c});
+        G[b].push_back({a,c});
+    }
+
+    int v1,v2;
+    cin >> v1 >> v2;
+
+    int arr1[4] = {1,v1,v2,N};
+    int arr2[4] = {1,v2,v1,N};
+
+    int ans, idx(0), flag(0);
+    for(int i = 0; i < 3; i++) {
+        Dijkstra(arr1[i],arr1[i+1]);
+        if(D[arr1[i+1]] == -1) {
+            flag++;
+            idx = 0x3f3f3f3f;
+            break;
+        }
+        idx += D[arr1[i+1]];
+    }
+    ans = idx;
+
+    idx = 0;
+    for(int i = 0; i < 3; i++) {
+        Dijkstra(arr2[i],arr2[i+1]);
+        if(D[arr2[i+1]] == -1) {
+            flag++;
+            idx = 0x3f3f3f3f;
+            break;
+        }
+        idx += D[arr2[i+1]];
+    }
+    ans = min<int>(idx,ans);
+    
+    if(flag == 2) {
+        cout << -1;
+        return 0;
+    }
+    cout << ans;
+    return 0;
+}
